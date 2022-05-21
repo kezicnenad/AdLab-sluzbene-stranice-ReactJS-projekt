f7b84de47c4f68fc026551f91f5665a335909869
Kežić Nenad <kezicnenad@gmail.com>
2022-05-21 16:46:25 +0200

    AdLab

--- src/components/About.js
+++ src/components/About.js
@@ -9,10 +9,16 @@ function Second() {
       <div className="container">
         <div className="row">
           <div className="col-12 col-lg-6 services-area padding-top padding-bottom">
-            <div className="purple-overlay" style={{backgroundImage: 'url(' + Slika1 + ')', backgroundPosition: 'center', backgroundSize: 'cover'}}>
-            </div>
-            <div className="row no-gutters wow fadeInLeft">
-            </div>
+            <div
+              className="purple-overlay"
+              style={{
+                backgroundImage: "url(" + Slika1 + ")",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                borderRight: '15px solid white'
+              }}
+            ></div>
+            <div className="row no-gutters wow fadeInLeft"></div>
           </div>
           <div className="col-12 col-lg-6 about-area padding-top padding-bottom text-center text-lg-left">
             <div className="about-content wow fadeInRight">
@@ -33,7 +39,7 @@ function Second() {
                   typesetting. Lorem Ipsum has been the industry’s standard
                   dummy. Lorem Ipsum has been the industry’s standard dummy.
                 </p>
-                
+
                 <a
                   href="#about-sec"
                   id="we-are"
